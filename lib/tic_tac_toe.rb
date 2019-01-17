091243acadb9cb696f49c47fb59ad2932a29e6cc
Dana Simmons <dsanalog@gmail.com>
2019-01-17 23:44:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,3 +38,7 @@ class TicTacToe
   def position_taken?(board, location)
     board[location] != " " && board[location] != ""
   end
+  def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+  end
+  
